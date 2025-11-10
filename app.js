b9bbd09aa51084987b4da2d0d5169de5c3ca6e76
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-11-10 20:47:40 +0100

    this is my frist mobile dev project

---
+++ app.js
@@ -0,0 +1,13 @@
+// functions , 
+// conditions and decision 
+// constant 
+// const 
+// storing infomaiton inside js 
+// Working with DOM
+// How javascript work behind the scence 
+// How to comment in javascript 
+//  how to work with high order array to make your code cleaner
+// Event loop , Callback ,
+// asynchronous in javascript ---------- is for working with external product/serivecs 
+// github . used for sharing , and seeing our work 
+ // knowing github commands 
